5954586c95a7bab66701c4bbdf1a6a6073fade5d
Santosh910 <138295487+Santosh910@users.noreply.github.com>
2023-10-25 13:09:40 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import { Hello } from './Controllers/GlobalControllers.js'
 
 const app = express()
 
+app.get ("/", function(req, res){
+    res.send('hello...')
+})
+
 app.get('/Hello',Hello)
 
 app.listen(8000,() =>console.log("app is running on 8000"))
