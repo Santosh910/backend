92d119e10b2563b99d75456005e9baa251a83d87
Santosh910 <138295487+Santosh910@users.noreply.github.com>
2023-10-27 23:53:05 +0530

    update

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 import express from 'express'
 // import { Hello } from './Controllers/GlobalControllers.js'
 import router from './Routes/index.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import morgan from 'morgan';
 
 const app = express()
+dotenv.config();
+app.use(morgan('dev'))
+
+
+app.use((req,res,next) =>{
+    console.log("hi from middleware use")
+    // res.send("hi  from middleware use")
+    next();
+})
 
 app.get("/", function (req, res) {
     res.send('hello santosh...')
 })
 
 app.use("/api/v1", router)
+
+mongoose.connect('mongodb+srv://survesantosh09:67kDdxwCIBYPytGO@cluster0.hhewsjk.mongodb.net/Awdiz').then(() => console.log('database connected!'))
+
+// mongoose.connect(process.env.MONGOURL).then(() => console.log("Database connected"))
+
 
 app.listen(8000, () => console.log("app is running on 8000"))
