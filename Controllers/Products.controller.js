f8fe31e5fa571fd70944a0eb556a535376211013
Santosh910 <138295487+Santosh910@users.noreply.github.com>
2023-11-03 21:44:26 +0530

    update

--- Controllers/Products.controller.js
+++ Controllers/Products.controller.js
@@ -1,11 +1,32 @@
-export const getAllProduct = (req,res) => {
-   res.send("All products.....")
+
+import productModel from "../Modals/product.model.js"
+
+export const getAllProduct = (req, res) => {
+    res.send("All products.....")
 }
 
-export const getSingleProduct = (req,res) =>{
+export const getSingleProduct = (req, res) => {
     res.send("Single Products..... ")
-} 
+}
 
-export const addProduct = (req,res) => {
-    res.send("product added.....")
+export const addProduct = async (req, res) => {
+    try {
+        const { name, price, category, image, id } = req.body;
+        if (!name || !price || !category || !image) return res.status(404).json({ success: false, message: "all fields mandotory" })
+
+        const product = new productModel({
+            name,
+            price,
+            category,
+            image: image,
+            userId: id
+        })
+        console.log(product, "-product here")
+        const ress = await product.save();
+        console.log(ress, "respone from mongodb ")
+        return res.status(200).json({ success: true, message: "product added successfully" })
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error })
+    }
+
 }
